188c8f21644305c89ab28e93389eaf049c9aa533
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-09 22:37:25 +0530

    Comparision Operators

---
+++ comparision_oprt.js
@@ -0,0 +1,21 @@
+let x = 10;
+let y = 20;
+
+console.log(x == y); //equal to
+
+console.log(x != y); //not equal to
+
+console.log(x === y); //equal value and type
+
+console.log(x !== y); //not equal value and type
+
+console.log(x > y); //greater than
+
+console.log(x < y); //less than
+
+console.log(x >= y); //Greater than equal to
+
+console.log(x <= y); //lesser than equal to
+
+
+
