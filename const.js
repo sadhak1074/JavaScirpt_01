e26deb5f1f7c430956ce8a909652f8e72d604aa3
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-09 18:34:56 +0530

    const

---
+++ const.js
@@ -0,0 +1,10 @@
+const a = 10
+
+//const a = 10 this will throw an error as const is used for costant values which can't be 
+//redeclared and updated once declared in the program;
+
+console.log(a)
+
+{
+    //const a = 10 can't even redeclare or update it in a block it'll again throw an error.
+}
