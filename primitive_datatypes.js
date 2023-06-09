255dcec12d7657aadd8276ea73ea19e5420d13be
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-09 18:52:20 +0530

    primitive datatypes

---
+++ primitive_datatypes.js
@@ -0,0 +1,13 @@
+//primitive datatypes are predefined datatypes
+
+let a = "sadhak";
+let b = 345;
+let c = true; //can also be false
+let d = BigInt("5909");
+let e = undefined;
+let f = null;
+let g = Symbol("I am a symbol");
+
+console.log(a, b, c, d, e, f, g)
+
+console.log(typeof(a)) //can find which type of datatype the variable holds using 'typeof'
