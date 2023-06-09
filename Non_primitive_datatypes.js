9a4a2486c8481371b56b7f2681d3caded19e6a96
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-09 19:06:56 +0530

    Non-Primitive DataTypes

---
+++ Non_primitive_datatypes.js
@@ -0,0 +1,12 @@
+const roll_no_1 = {
+    name: 'sadhak saini',
+    phone_no: 8000287465,
+    marks: 100/100
+}
+const roll_no_2 = {
+    name: 'vishnu dhaked',
+    phone_no: 8003338588,
+    marks: 100/100
+}
+
+console.log(roll_no_1)
