ff7514525fdef49bc62f360859c40e6b3c5fe001
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-09 21:54:36 +0530

    Assingment Operators

---
+++ assingment_operator.js
@@ -0,0 +1,18 @@
+//Assignment operators used to assign values to variables.
+
+let x = 10;
+let y = 10;
+
+console.log(x = y); //same as x = y 
+
+console.log(x += y);//same as x = x + y 
+
+console.log(x -= y);//same as x = x - y 
+
+console.log(x *= y);//same as x = x * y 
+
+console.log(x /= y);//same as x = x / y 
+
+console.log(x %= y);//same as x = x % y 
+
+console.log(x **= y);//same as x = x ** y 
