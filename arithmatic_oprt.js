598a232bc6dadcb86ec5989f4e896a35f2a47b9b
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-09 21:40:58 +0530

    Arithmatic Operators

---
+++ arithmatic_oprt.js
@@ -0,0 +1,15 @@
+//Arithmatic Operators(Used for mathmatical operations).
+let x = 10;
+let y = 20;
+
+console.log(x+y); //Addition operator '+'
+
+console.log(x-y); //Subtraction Operator '-'
+
+console.log(x*y); //Multiplication Operator '*'
+
+console.log(x/y); //Division Operator '/'
+
+console.log(x%y); //Modulo Operater'%'it gives us remainder.
+
+console.log(x**y); //Exponential operator'**' it gives the value of x^y.
