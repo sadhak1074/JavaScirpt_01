bd2997d26f02aa66c2e0643a6084768b0c10639f
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-10 20:57:50 +0530

    Switch-Case in Java Script

---
+++ Switch_case.js
@@ -0,0 +1,13 @@
+let fruit = 'apple';
+
+switch (fruit) {
+    case 'banana':
+        console.log('It is a banana');
+        break;
+    case 'apple':
+        console.log('I love apple');
+        break;
+    case 'orange':
+        console.log('I love orange');
+        break;
+    }
