5ac211b06a5445affba1d311472cf5548d9aeceb
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-20 15:02:55 +0530

    Escape Sequence

---
+++ Escape_ssequence.js
@@ -0,0 +1,15 @@
+//we can use a single qoute to use single qoute between single qoutes.
+let name = 'Adam D\'Angelo';
+console.log(name);
+
+// "/n" can be used for a new line.
+let paragraph = "my name is sadhak \nsaini";
+console.log(paragraph);
+
+// "\t" tab space, 4 spaces in total.
+let line = "I am persuing my bca from the\t ICFAI University"
+console.log(line);
+
+// "\r" for carriage return 
+let line2 = "I want to become an \rData Scientist";
+console.log(line2);
