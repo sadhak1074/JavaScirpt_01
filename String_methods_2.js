242517957a94e55c09295d05ee50e486832a108f
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-20 21:07:32 +0530

    Strings methods 2

---
+++ String_methods_2.js
@@ -0,0 +1,13 @@
+let name1 = "Sadhak";
+let name2 = " Saini";
+let name3 = name1.concat(name2, " yes");
+
+console.log(name3);
+
+let name4 = "   Harry";
+let name5=name4.trim();
+console.log(name5);
+
+let name = "harry";
+console.log(name[0]);
+console.log(name[1]);
