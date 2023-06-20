b651286efb0962b721ceb680c3fe80a2bf1c7a77
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-20 20:56:23 +0530

    String Methods

---
+++ String_methods.js
@@ -0,0 +1,22 @@
+let name = "Sadhak";
+console.log(name.length); //Prints the length
+
+let age = "twenty";
+console.log(name.toUpperCase()); //Upper-casing
+
+let name2 = "HANUMAN";
+console.log(name2.toLowerCase()); //Lower-casing
+
+let name3 = "Jackson";
+console.log(name3.slice(2, 4));
+// Prints 'ck' from index position of two to four, four excluded
+
+console.log(name3.slice(2));
+//prints 2 to end
+
+let name4 = "sadhak bhai";
+console.log(name4.replace("bhai", "bhau"));
+//Replaces "bhai" in name4 with "bhau".
+
+
+
