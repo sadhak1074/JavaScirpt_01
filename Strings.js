2ca9a9dc29ec75c7df7bd2a3fee50f23e0c514cb
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-20 14:53:57 +0530

    Strings

---
+++ Strings.js
@@ -0,0 +1,18 @@
+let name = "Sadhak" //creates a string
+console.log(name);
+
+console.log(name.length);//Prints length of a string
+
+//We can also declare strings by using single qoutrs (' ')
+let pc_name = 'Dell'
+
+console.log(pc_name);
+
+//template literals
+let fame = `Hall 'of' "of" fame`;
+console.log(fame);
+
+//inserting variable directly in a template litral
+
+let fav_character = "Damon Salvatore";
+console.log(`My fav character is ${fav_character}`);
