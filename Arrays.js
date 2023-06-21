fb0ff889bff0c1f047abcbb8178a0f96e65eecfd
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-21 20:47:40 +0530

    Arrays

---
+++ Arrays.js
@@ -0,0 +1,25 @@
+//Declaring arrays
+const fruits = ["Banana", "Apple", "Grapes"];
+const a = [1, "Sadhak", true];
+//Elements inside an array can be of different datatypes
+
+//Printing the arrays
+console.log(fruits);
+console.log(a);
+
+//Accessing values of the arrays
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers[0]); //prints first element in the list
+console.log(numbers[4]); //prints last element in the list
+
+//finding the length
+console.log(numbers.length); //prints length of given numbers
+
+//Changing the values in given array
+console.log(numbers[2] = 8);
+
+//typeof operator in arrays
+const n = [1, 4, 5];
+
+console.log(typeof n);//it will return object as it stores many values in single variable
+
